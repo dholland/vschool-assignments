4760ae48307eb700f145dfce8c13210ddedd6723
Danny Holland <holland.danny@gmail.com>
2020-04-13 09:56:55 -0600

    Completed Node Calculator

---
+++ 01_module-1/05_noce-calculator/app.js
@@ -0,0 +1,50 @@
+//Assignment https://coursework.vschool.io/javascript-calculator/
+
+const readline = require('readline-sync');
+
+//Ask user for questions
+const number1 = readline.questionInt('May I have your first number? ');
+const number2 = readline.questionInt('May I have your second number? ');
+
+//Convert user response to number
+const num1 = Number(number1);
+const num2 = Number(number2);
+
+// console.log(num1, num2);
+
+//Ask user to pick a type of operation using the keyInSelect
+const operator = ['Add', 'Subtract', 'Multiply', 'Divide'],
+	index = readline.keyInSelect(operator, 'Which Operation?');
+
+console.log('Ok, I will ' + operator[index] + 'your two numbers');
+
+//run function based on operator[index] match
+
+if (operator[index] == 'Add') {
+	add();
+} else if (operator[index] == 'Subtract') {
+	sub();
+} else if (operator[index] == 'Multiply') {
+	mul();
+} else div();
+
+//Operation functions
+
+function add() {
+	let result = num1 + num2;
+	console.log(result);
+}
+function sub() {
+	let result = num1 - num2;
+	console.log(result);
+}
+
+function mul() {
+	let result = num1 * num2;
+	console.log(result);
+}
+
+function div() {
+	let result = num1 / num2;
+	console.log(result);
+}
