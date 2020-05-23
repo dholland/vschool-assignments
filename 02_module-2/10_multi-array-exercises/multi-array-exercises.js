1a1ea5601aaf7ecdf2571feb875fd95ca0a1899c
Danny Holland <holland.danny@gmail.com>
2020-05-23 14:32:23 -0600

    multi array exercises

---
+++ 02_module-2/10_multi-array-exercises/multi-array-exercises.js
@@ -0,0 +1,66 @@
+var peopleArray = [
+	{
+		firstName: 'Sarah',
+		lastName: 'Palin',
+		age: 47,
+	},
+	{
+		firstName: 'Frank',
+		lastName: 'Zappa',
+		age: 12,
+	},
+	{
+		firstName: 'Rick',
+		lastName: 'Sanchez',
+		age: 78,
+	},
+	{
+		firstName: 'Morty',
+		lastName: 'Smith',
+		age: 29,
+	},
+	{
+		firstName: 'Kyle',
+		lastName: 'Mooney',
+		age: 27,
+	},
+	{
+		firstName: 'Pasha',
+		lastName: 'Datsyuk',
+		age: 13,
+	},
+	{
+		firstName: 'Lev',
+		lastName: 'Tolstoy',
+		age: 82,
+	},
+];
+
+function sortedOfAge(arr) {
+	return arr
+		.sort(function (a, b) {
+			if (a.lastName < b.lastName) return -1;
+			if (a.lastName > b.lastName) return 1;
+			return 0;
+		})
+		.reduce(function (final, person) {
+			let fullName = `${person.firstName} ${person.lastName}`;
+			if (person.age > 18) {
+				final.push(`<li>${fullName} is ${person.age}</li>`);
+			}
+			return final;
+		}, []);
+}
+
+console.log(sortedOfAge(peopleArray));
+
+/*
+ Output: 
+ [ 
+     "<li>Kyle Mooney is 27</li>",
+     "<li>Sarah Palin is 47</li>",
+     "<li>Rick Sanchez is 78</li>",
+     "<li>Morty Smith is 29</li>",
+     "<li>Lev Tolstoy is 82</li>" 
+ ]
+ */
