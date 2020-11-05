732b75fea01377ea2ca44f41e7e7b1ab15852568
Danny Holland <holland.danny@gmail.com>
2020-11-04 23:48:29 -0700

    completed Dice project

--- 03_module-3/04_dj-react/src/App.js
+++ 03_module-3/04_dj-react/src/App.js
@@ -6,15 +6,56 @@ import Square from './components/Square';
 
 export default class App extends Component {
 	state = {
-		colors: ['white', 'black', 'white', 'black', 'white'],
+		colors: [
+			'white',
+			'white',
+			'white',
+			'white',
+			'black',
+			'black',
+			'black',
+			'black',
+		],
 	};
 
 	toggleColor = () => {
 		this.setState((prevState) => {
+			console.log(prevState);
 			let newState = prevState.colors.map((color) => {
 				return color === 'white' ? 'black' : 'white';
 			});
 			return { colors: newState };
+		});
+		console.log('toggle ended');
+	};
+
+	purple = () => {
+		this.setState((prevState) => {
+			let newState = prevState.colors.map((color, idx) => {
+				if (idx < 4) {
+					return 'purple';
+				} else if (color === 'purple') {
+					return 'white';
+				} else {
+					return color;
+				}
+			});
+			console.log(newState);
+			return { colors: newState };
+		});
+	};
+	reset = () => {
+		this.setState({
+			colors: [
+				'white',
+				'white',
+				'white',
+				'white',
+				'black',
+				'black',
+				'black',
+				'black',
+			],
 		});
 	};
 
@@ -22,13 +63,16 @@ export default class App extends Component {
 		const squares = this.state.colors.map((color, idx) => {
 			return <Square key={idx} color={color} />;
 		});
+		// console.log('State is:', this.state.state);
+
 		return (
-			<div className='container'>
-				<div className='buttons'>
+			<div className='container columns'>
+				<div className='controls'>
 					<button onClick={this.toggleColor}>Toggle Color</button>
-					<button>Button 2</button>
+					<button onClick={this.purple}>Purple Color</button>
+					<button onClick={this.reset}>Reset</button>
 				</div>
-				<div className='break'></div>
+
 				<div className='pad'>{squares}</div>
 			</div>
 		);
